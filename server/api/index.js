66e8bdaeaf4278384c5e9b46eb6952db4fa77f82
Valeriia Harmash <valeriaharmash@gmail.com>
2023-03-17 17:28:09 -0400

    created express routes to search for cities, destinations and activities by categories

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,14 @@
-const router = require('express').Router();
-const { authRouter, usersRouter, destinationsRouter } = require('./routes');
+const router = require("express").Router();
+const { authRouter, usersRouter, destinationsRouter, activitiesRouter } = require("./routes");
 
 // /api/{path}
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
-router.use('/destinations', destinationsRouter);
+router.use("/auth", authRouter);
+router.use("/users", usersRouter);
+router.use("/destinations", destinationsRouter);
+router.use("/activities", activitiesRouter);
 
 router.use((req, res, next) => {
-  const err = new Error('API route not found');
+  const err = new Error("API route not found");
   err.status = 404;
   next(err);
 });
--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,9 +1,11 @@
-const authRouter = require('./auth');
-const usersRouter = require('./users');
-const destinationsRouter = require('./destinations');
+const authRouter = require("./auth");
+const usersRouter = require("./users");
+const destinationsRouter = require("./destinations");
+const activitiesRouter = require("./activities");
 
 module.exports = {
   authRouter,
   usersRouter,
   destinationsRouter,
+  activitiesRouter
 };
